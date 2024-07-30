8b8b76a9335bd801685f4c99e0df4f5193508f40
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2024-07-29 23:25:42 -0300

    first commit

---
+++ src/test/java/br/com/nt/easystay/EasystayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nt.easystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasystayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
